4916bafb387f39f34c4b91642d5a2f6fa6f51533
Dinith Hasaranga <dinith.dash@gmail.com>
2024-10-30 21:27:56 +0530

    Init

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Smooth scroll to sections
+function scrollToAbout() {
+  document.querySelector("#about").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToContact() {
+  document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
+}
