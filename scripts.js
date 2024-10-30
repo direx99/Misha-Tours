75cf43bb4ac3d21d35d6fcc4808c6e71cf004b76
Dinith Hasaranga <dinith.dash@gmail.com>
2024-10-30 21:39:31 +0530

    Minor changes

--- scripts.js
+++ scripts.js
@@ -6,3 +6,12 @@ function scrollToAbout() {
 function scrollToContact() {
   document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
 }
+
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar-custom");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
